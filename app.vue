c923518a53da505ff7fbdb0503a25327b6190e04
danimog <daniele.moggia@gmail.com>
2024-09-30 18:01:37 +0200

    inizio

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <template>
+      <div class="container mx-auto mt-10">
+        <h1 class="text-4xl text-center font-bold">
+          Hello world!
+        </h1>
+      </div>
+    </template>
+  </div>
+</template>
