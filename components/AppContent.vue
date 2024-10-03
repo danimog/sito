0fcb00468ccbff830bc03a3c7f53e211885015b7
danimog <daniele.moggia@gmail.com>
2024-10-03 15:05:33 +0200

    bio ema

--- components/AppContent.vue
+++ components/AppContent.vue
@@ -1,6 +1,23 @@
 <template>
     <div class="container mx-auto my-10 p-3">
         <div class="text-5xl font-extrabold text-center">
+            <span class="bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-green-700">
+                La mia storia
+            </span>
+        </div>
+        <p class="my-5 lg:px-20 lg:text-xl">
+            Dopo la <strong>Laurea in Ingegneria delle Telecomunicazioni</strong> ho conseguito il Master Universitario in
+            Geomatica Marina all’Istituto Idrografico della Marina Militare. Ho lavorato nell’ambito della
+            acustica subacquea, della Difesa e delle energie rinnovabili.<br>
+            Sono da sempre impegnato nel <strong>sociale e nell’associazionismo cattolico</strong>, ho avuto importanti
+            esperienze di cooperazione internazionale in Africa e nelle favelas del Brasile.<br>
+            Sono stato per dieci anni <strong>Sindaco alle Cinque Terre, ricoprendo anche il ruolo di vice Presidente del
+            Parco Nazionale.</strong><br>
+            Idealista e sognatore, ma sempre pragmatico e con i piedi ben piantati per terra, non ho dubbi che
+            il mondo si possa cambiare in meglio, perché il futuro è nelle mani e nei cuori di chi, con impegno,
+            costanza e visione, si rende disponibile a lavorare per <strong>costruire il bene comune</strong>.
+        </p>
+        <div class="text-5xl font-extrabold text-center">
             <span class="bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-600">
                 In Regione, uno di Noi
             </span>
@@ -29,25 +46,30 @@
             </span>
         </div>
         <p class="my-5 lg:px-20 lg:text-xl">
-            Il bene del nostro territorio, dove la <strong>sanità</strong>, il <strong>lavoro</strong>, le <strong>infrastrutture</strong>, il
-            sostegno alle <strong>famiglia</strong>, le politiche per l’<strong>imprenditoria</strong> giovanile, l’attenzione
-            agli <strong>anziani</strong> e non ultima la <strong>cura del nostro territorio</strong>, sono i valori al centro del
+            Il bene del nostro territorio, dove la <strong>sanità</strong>, il <strong>lavoro</strong>, le
+            <strong>infrastrutture</strong>, il
+            sostegno alle <strong>famiglia</strong>, le politiche per l’<strong>imprenditoria</strong> giovanile,
+            l’attenzione
+            agli <strong>anziani</strong> e non ultima la <strong>cura del nostro territorio</strong>, sono i valori al
+            centro del
             mio impegno per migliorare il nostro territorio, fragile ma con grandi potenzialità.<br><br>
             Ogni giorno, per tutto l’anno, sento la difficoltà di vivere in un <strong>territorio isolato</strong> e
             la responsabilità di questa emarginazione troppo spesso è il risultato di <strong>scelte
-            amministrative portate avanti da chi non ha saputo ascoltare le esigenze
-            reali e quotidiane dei cittadini</strong> che non vedono riconosciute le loro
+                amministrative portate avanti da chi non ha saputo ascoltare le esigenze
+                reali e quotidiane dei cittadini</strong> che non vedono riconosciute le loro
             professionalità e a fatica si possono muovere; abitanti che per curarsi spesso
             devono fare chilometri; famiglie sulle quali pesa la cura degli anziani e dei
             bambini, perché mancano strutture adeguate soprattutto nei piccoli centri e in
             alta stagione; giovani che scelgono di allontanarsi perché le possibilità lavorative
             troppo spesso sono solo stagionali; pescatori e contadini emarginati in nome di
             scelte che prescindono dalla tutela dell’ambiente.<br><br>
-        <strong>Io mi voglio impegnare per riportare il nostro territorio al centro del tavolo
-            politico.</strong><br><br>
+            <strong>Io mi voglio impegnare per riportare il nostro territorio al centro del tavolo
+                politico.</strong><br><br>
             Una <strong>provincia</strong> dove anche il suo <strong>capoluogo</strong> patisce <strong>l’impoverimento
-            generale delle infrastrutture:</strong> il sovraffollamento dei <strong>treni</strong>, il rincaro degli <strong>affitti</strong>,
-            l’incuria delle <strong>strade provinciali</strong> e, non ultima, una <strong>sanità</strong> pubblica e centrale
+                generale delle infrastrutture:</strong> il sovraffollamento dei <strong>treni</strong>, il rincaro degli
+            <strong>affitti</strong>,
+            l’incuria delle <strong>strade provinciali</strong> e, non ultima, una <strong>sanità</strong> pubblica e
+            centrale
 
             che patisce la carenza di Punti di Primo Intervento di prossimità e dove ogni
             giorno è una lotta per mantenere quelli esistenti, come quello di Levanto; dove il
